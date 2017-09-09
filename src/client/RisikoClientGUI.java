0c79a9c642b9638330081ec9f7d3f2da1ef9548f
Trustet <yannik@trustet.de>
2017-09-09 22:25:34 +0200

    TODO

--- src/client/RisikoClientGUI.java
+++ src/client/RisikoClientGUI.java
@@ -1,8 +1,6 @@
 //TODO Javadoc
-//TODO spiel erstellen, wenn schon eins ist sperren, exception gibt es schon
 //TODO Laden bei SpielerMission
 //TODO speichern/Laden mitten in zug
-//TODO servererror wenn nach geladenem spiel normales gestartet wird(manchmal? :D)
 //TODO Optionen im Menü kann raus?
 
 package client;
